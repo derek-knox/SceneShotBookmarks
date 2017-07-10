57f8815c044462c216c9e76cb7d78c0829b3074e
Braindrop Labs <braindroplabs@gmail.com>
2017-07-09 17:16:55 -0700

    added ScriptableObject script

---
+++ SceneShotBookmarksScriptableObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SceneShotBookmarksScriptableObject : ScriptableObject {
+    
+    private const int DEFAULT_BOOKMARK_COUNT = 3;
+    
+    public List<SceneShotBookmarkModel> sceneShotBookmarks;
+
+    public SceneShotBookmarksScriptableObject()
+    {
+    	initModels();
+    }
+
+    private void initModels()
+    {
+    	// Prep sceneShotBookmarks creation
+    	sceneShotBookmarks = new List<SceneShotBookmarkModel>();
+    	SceneShotBookmarkModel model;
+    	for(int i = 0; i < DEFAULT_BOOKMARK_COUNT; i++)
+    	{
+    		// Create and add model
+    		model = new SceneShotBookmarkModel();
+    		sceneShotBookmarks.Add(model);
+
+    		// Update model
+    		model.nickname = "nickname";
+			model.position = new Vector3(0f, 0f, 0f);
+			model.rotation = Quaternion.identity;
+			model.orthographic = false;
+			model.orthographicSize = 0;
+    	}
+    }
+}
